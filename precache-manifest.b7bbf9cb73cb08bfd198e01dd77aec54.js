a18dd4ee0406f838c42baec36afd86548ac274f1
Wasiq Qamar <wasiq.qbutt@gmail.com>
2020-04-06 01:56:42 +0500

    Updates

---
+++ precache-manifest.b7bbf9cb73cb08bfd198e01dd77aec54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e2ff45c8a4b036a7ba1a88adc9657c",
+    "url": "/communally/index.html"
+  },
+  {
+    "revision": "506da5e5fbc00b336fac",
+    "url": "/communally/static/css/main.c0d4b490.chunk.css"
+  },
+  {
+    "revision": "c3d154ec9c7725e5b8b8",
+    "url": "/communally/static/js/2.41c0bb5a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/communally/static/js/2.41c0bb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "506da5e5fbc00b336fac",
+    "url": "/communally/static/js/main.b3a3db80.chunk.js"
+  },
+  {
+    "revision": "24a1189f2e5dda6735b0",
+    "url": "/communally/static/js/runtime-main.10c4d9cd.js"
+  },
+  {
+    "revision": "d86002636879eda954464bc94aa18e86",
+    "url": "/communally/static/media/no-img.d8600263.png"
+  }
+]);
